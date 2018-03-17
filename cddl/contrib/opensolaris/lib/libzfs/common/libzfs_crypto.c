de2fd1b6b0a8d21330827355bb0889cf3d93cbdf
Sean Eric Fagan <sef@Testy.local>
2018-03-16 22:25:52 -0700

    I swapped the order of arguments, and also missed the different semantics in return value.  This allows passphrase-created encrypted datasets to work.

--- cddl/contrib/opensolaris/lib/libzfs/common/libzfs_crypto.c
+++ cddl/contrib/opensolaris/lib/libzfs/common/libzfs_crypto.c
@@ -436,6 +436,17 @@ pbkdf2(uint8_t *passphrase, size_t passphraselen, uint8_t *salt,
     size_t outputlen)
 {
 	int ret = 0;
+#ifdef __FreeBSD__
+	ret = PKCS5_PBKDF2_HMAC_SHA1((char *)passphrase, passphraselen,
+	    salt, saltlen, iterations,
+	    outputlen, output);
+	/*
+	 * PKCS5_PBKDF2_HMAC_SHA1 returns 1 on success, but 0 on error,
+	 * while our calling function expects 0 on success and non-zero
+	 * on error.  So we just toggle it.
+	 */
+	ret = !ret;
+#else
 	CK_SESSION_HANDLE session;
 	char *tmpkeydata = NULL;
 	size_t tmpkeydatalen = 0;
@@ -473,6 +484,7 @@ pbkdf2(uint8_t *passphrase, size_t passphraselen, uint8_t *salt,
 	(void) memcpy(output, tmpkeydata, tmpkeydatalen);
 	(void) memset(tmpkeydata, 0, tmpkeydatalen);
 	free(tmpkeydata);
+#endif /* __FreeBSD__ */
 
 	return (ret);
 }
