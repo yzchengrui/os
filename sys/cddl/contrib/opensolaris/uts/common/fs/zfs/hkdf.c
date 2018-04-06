02fec1173fc375fa6ebfee7f8374b642eff5cf06
Sean Eric Fagan <sef@kithrup.com>
2018-04-06 16:03:29 -0700

    Initial commit for an attempt to port the OpenZFS crypto changes from ICP to using FreeBSD's existing crypto code.
    This builds, but won't work -- I haven't changed the actual
    encryption code yet.

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/hkdf.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/hkdf.c
@@ -19,8 +19,12 @@
 
 #include <sys/dmu.h>
 #include <sys/hkdf.h>
-#include <sys/crypto/api.h>
-#include <sys/sha2.h>
+#ifdef __FreeBSD__
+# include <sys/freebsd_crypto.h>
+#else
+# include <sys/crypto/api.h>
+# include <sys/sha2.h>
+#endif
 #include <sys/hkdf.h>
 
 static int
@@ -28,20 +32,27 @@ hkdf_sha512_extract(uint8_t *salt, uint_t salt_len, uint8_t *key_material,
     uint_t km_len, uint8_t *out_buf)
 {
 	int ret;
+#ifndef __FreeBSD__
 	crypto_mechanism_t mech;
-	crypto_key_t key;
 	crypto_data_t input_cd, output_cd;
+#endif
+	crypto_key_t key;
 
+#ifndef __FreeBSD__
 	/* initialize HMAC mechanism */
 	mech.cm_type = crypto_mech2id(SUN_CKM_SHA512_HMAC);
 	mech.cm_param = NULL;
 	mech.cm_param_len = 0;
-
+#endif
+	
 	/* initialize the salt as a crypto key */
 	key.ck_format = CRYPTO_KEY_RAW;
 	key.ck_length = CRYPTO_BYTES2BITS(salt_len);
 	key.ck_data = salt;
 
+#ifdef __FreeBSD__
+	crypto_mac(&key, NULL, 0, out_buf, SHA512_DIGEST_LENGTH);
+#else
 	/* initialize crypto data for the input and output data */
 	input_cd.cd_format = CRYPTO_DATA_RAW;
 	input_cd.cd_offset = 0;
@@ -58,6 +69,7 @@ hkdf_sha512_extract(uint8_t *salt, uint_t salt_len, uint8_t *key_material,
 	ret = crypto_mac(&mech, &input_cd, &key, NULL, &output_cd, NULL);
 	if (ret != CRYPTO_SUCCESS)
 		return (SET_ERROR(EIO));
+#endif
 
 	return (0);
 }
@@ -67,10 +79,14 @@ hkdf_sha512_expand(uint8_t *extract_key, uint8_t *info, uint_t info_len,
     uint8_t *out_buf, uint_t out_len)
 {
 	int ret;
+#ifdef __FreeBSD__
+	struct hmac_ctx ctx;
+#else
 	crypto_mechanism_t mech;
 	crypto_context_t ctx;
-	crypto_key_t key;
 	crypto_data_t T_cd, info_cd, c_cd;
+#endif
+	crypto_key_t key;
 	uint_t i, T_len = 0, pos = 0;
 	uint8_t c;
 	uint_t N = (out_len + SHA512_DIGEST_LENGTH) / SHA512_DIGEST_LENGTH;
@@ -79,16 +95,19 @@ hkdf_sha512_expand(uint8_t *extract_key, uint8_t *info, uint_t info_len,
 	if (N > 255)
 		return (SET_ERROR(EINVAL));
 
+#ifndef __FreeBSD__
 	/* initialize HMAC mechanism */
 	mech.cm_type = crypto_mech2id(SUN_CKM_SHA512_HMAC);
 	mech.cm_param = NULL;
 	mech.cm_param_len = 0;
+#endif
 
 	/* initialize the salt as a crypto key */
 	key.ck_format = CRYPTO_KEY_RAW;
 	key.ck_length = CRYPTO_BYTES2BITS(SHA512_DIGEST_LENGTH);
 	key.ck_data = extract_key;
 
+#ifndef __FreeBSD__
 	/* initialize crypto data for the input and output data */
 	T_cd.cd_format = CRYPTO_DATA_RAW;
 	T_cd.cd_offset = 0;
@@ -105,29 +124,49 @@ hkdf_sha512_expand(uint8_t *extract_key, uint8_t *info, uint_t info_len,
 	info_cd.cd_length = info_len;
 	info_cd.cd_raw.iov_base = (char *)info;
 	info_cd.cd_raw.iov_len = info_cd.cd_length;
-
+#endif
+	
 	for (i = 1; i <= N; i++) {
 		c = i;
 
+#ifdef __FreeBSD__
+		crypto_mac_init(&ctx, &key);
+#else
 		T_cd.cd_length = T_len;
 		T_cd.cd_raw.iov_len = T_cd.cd_length;
 
 		ret = crypto_mac_init(&mech, &key, NULL, &ctx, NULL);
 		if (ret != CRYPTO_SUCCESS)
 			return (SET_ERROR(EIO));
+#endif
 
+#ifdef __FreeBSD__
+		crypto_mac_update(&ctx, T, T_len);
+#else
 		ret = crypto_mac_update(ctx, &T_cd, NULL);
 		if (ret != CRYPTO_SUCCESS)
 			return (SET_ERROR(EIO));
+#endif
 
+#ifdef __FreeBSD__
+		crypto_mac_update(&ctx, info, info_len);
+#else
 		ret = crypto_mac_update(ctx, &info_cd, NULL);
 		if (ret != CRYPTO_SUCCESS)
 			return (SET_ERROR(EIO));
+#endif
 
+#ifdef __FreeBSD__
+		crypto_mac_update(&ctx, &c, 1);
+#else
 		ret = crypto_mac_update(ctx, &c_cd, NULL);
 		if (ret != CRYPTO_SUCCESS)
 			return (SET_ERROR(EIO));
+#endif
 
+#ifdef __FreeBSD__
+		crypto_mac_final(&ctx, T, SHA512_DIGEST_LENGTH);
+#else
 		T_len = SHA512_DIGEST_LENGTH;
 		T_cd.cd_length = T_len;
 		T_cd.cd_raw.iov_len = T_cd.cd_length;
@@ -135,6 +174,7 @@ hkdf_sha512_expand(uint8_t *extract_key, uint8_t *info, uint_t info_len,
 		ret = crypto_mac_final(ctx, &T_cd, NULL);
 		if (ret != CRYPTO_SUCCESS)
 			return (SET_ERROR(EIO));
+#endif
 
 		bcopy(T, out_buf + pos,
 		    (i != N) ? SHA512_DIGEST_LENGTH : (out_len - pos));
